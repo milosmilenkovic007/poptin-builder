2162527bd02c45bdb03a901397ac10c787599dce
odin3 <ascii@live.ru>
2017-07-17 08:29:49 +0300

    Use relative path

--- src/app/services/templates.service.ts
+++ src/app/services/templates.service.ts
@@ -12,7 +12,7 @@ export class TemplatesService {
   constructor(private http: Http) { }
 
   getTemplateHtml(templateName: string): Observable<string> {
-    return this.http.get(`/assets/templates/${templateName}.html`)
+    return this.http.get(`assets/templates/${templateName}.html`)
       .map((r: Response) => r.text())
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
