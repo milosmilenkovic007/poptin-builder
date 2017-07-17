270dc3108c18f663e3d6d568154aa3d3fbcedae1
odin3 <ascii@live.ru>
2017-07-17 03:24:23 +0300

    Add draggable and editable functionality

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 
 import { TemplatesService } from 'app/services/templates.service';
+
+// We need jQuery here because we use external templates HTML
+// And to take less size of HTML template do not clog up it with Angular tags
+// We will use dynamic watchers and bindings to DOM using jQuery
+const $ = window['jQuery'];
+
+const KEY_ESCAPE = 27;
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, AfterViewInit {
   templateHtml = '';
 
-  constructor(private tpl: TemplatesService) { }
+  public get hostElement() {
+    return this.elementRef.nativeElement;
+  }
+
+  constructor(private tpl: TemplatesService, private elementRef: ElementRef) { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
     this.tpl.getTemplateHtml('default')
       .subscribe(
         (template) => this.onTemplateFetch(template),
@@ -22,7 +37,36 @@ export class EditorComponent implements OnInit {
 
   onTemplateFetch(template: string) {
     this.templateHtml = template;
+    setTimeout(() => this.registerEditorHooks(), 150);
+  }
 
+  /**
+   * Set editable mode on template part
+   *
+   * @param {Event} event Event source
+   * @param {boolean} isEditable Is element editable
+   * @memberof EditorComponent
+   */
+  setEditableElementStatus(event: Event, isEditable: boolean) {
+    const element: HTMLElement = <HTMLElement> event.target;
+    element.contentEditable = isEditable.toString();
+  }
+
+  /**
+   * Register jQuery hooks on template markup
+   *
+   * @memberof EditorComponent
+   */
+  registerEditorHooks() {
+    $(this.hostElement)
+      .find('[data-control]')
+      .on('click', (event: MouseEvent) => this.setEditableElementStatus(event, true))
+      .on('keydown', (event: KeyboardEvent) => {
+        if (event.keyCode === KEY_ESCAPE) {
+          this.setEditableElementStatus(event, false);
+        }
+      })
+      .draggable();
   }
 
 }
